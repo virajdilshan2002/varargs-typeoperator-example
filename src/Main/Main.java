64d84420d36f3bdd9f2c16576c863278e975b166
viraj dilshan <virajdilshan2019@gmail.com>
2024-02-03 09:44:20 +0530

    varargs and typeopertor

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,10 +3,10 @@ package Main;
 import otherClasses.collection;
 public class Main {
     public static void main(String[] args) {
-        collection c1 = new collection(100,'A',"Hello!",50.75,true); //unbounded type
+        collection c1 = new collection(100,'A',"Hello!",50.75,true);
         c1.printCollection();
 
-        collection<Integer> c2 = new collection<>(100,200,300); //bounded type
+        collection<Integer> c2 = new collection<>(100,200,300); //compile time safety
         c2.printCollection();
     }
 }
